53623e109d0e91c5eaf114c8ebd801b60798b09f
Edvard <edvardfauchelevent@gmail.com>
2025-10-14 10:45:36 +0200

    Init project

---
+++ src/main/java/fr/kata/delivery/katadelivery/KataDeliveryApplication.java
@@ -0,0 +1,13 @@
+package fr.kata.delivery.katadelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KataDeliveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KataDeliveryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/kata/delivery/katadelivery/KataDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.kata.delivery.katadelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KataDeliveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
